2fc8d159fb4408297700e2719cb284abdaa003de
Emerson Harkin <emerson.f.harkin@gmail.com>
2020-02-25 23:26:18 -0500

    Implement Spike, SpikeMonitor, and VoltageMonitor components

--- rust/src/simulation/components.rs
+++ rust/src/simulation/components.rs
@@ -55,3 +55,69 @@ pub struct VoltageReset(pub NeuroFloat);
 impl Component for VoltageReset {
     type Storage = VecStorage<Self>;
 }
+
+pub struct Spike(pub DynamicalScalar<usize>);
+
+impl Spike {
+    pub fn new() -> Spike {
+        Spike(DynamicalScalar::new(0))
+    }
+}
+
+impl Component for Spike {
+    type Storage = VecStorage<Self>;
+}
+
+
+// MONITORS
+pub trait Monitor<T> {
+   fn write(&self, monitored_variable: T, current_time: TimeStep);
+}
+
+pub struct SpikeMonitor {
+    bufwriter: std::io::BufWriter<std::fs::File>,
+}
+
+impl SpikeMonitor {
+    pub fn new(output_file: std::fs::File) -> SpikeMonitor {
+        SpikeMonitor {
+            bufwriter: std::io::BufWriter::new(output_file)
+        }
+    }
+}
+
+impl Monitor<Spike> for SpikeMonitor {
+    fn write(&self, monitored_variable: Spike, current_time: TimeStep) {
+        use std::io::Write;
+        if monitored_variable.0.dynamical_get(current_time) > 0 {
+            writeln!(self.bufwriter, "{}", current_time);
+        }
+    }
+}
+
+impl Component for SpikeMonitor {
+    type Storage = VecStorage<Self>;
+}
+
+pub struct VoltageMonitor {
+    bufwriter: std::io::BufWriter<std::fs::File>,
+}
+
+impl VoltageMonitor {
+    pub fn new(output_file: std::fs::File) -> VoltageMonitor {
+        VoltageMonitor {
+            bufwriter: std::io::BufWriter::new(output_file)
+        }
+    }
+}
+
+impl Monitor<Voltage> for VoltageMonitor {
+    fn write(&self, monitored_variable: Voltage, current_time: TimeStep) {
+        use std::io::Write;
+        writeln!(self.bufwriter, "{}", monitored_variable.0.dynamical_get(current_time));
+    }
+}
+
+impl Component for VoltageMonitor {
+    type Storage = VecStorage<Self>;
+}
